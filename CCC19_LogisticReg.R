9b98513b966cf080c6ce329eff1b1900d31e5590
Charlene <q2330416@gmail.com>
2023-01-06 10:52:43 +0800

    Modify hcq to rem

--- CCC19_LogisticReg.R
+++ CCC19_LogisticReg.R
@@ -45,6 +45,7 @@ ccc19_lc$treatment_cs <- factor(ccc19_lc$treatment_cs, # Corticosteroid (CS)
                                 levels=c("Negative control", "Positive control", "Steroids alone", "Steroids+anything"))
 
 
+##**********************************************************************************************************************************##
 ##### Descriptive statistics #####
 ## Descriptive statistics, according to treatment_rem
 output0 <- summaryM(dead30 + age_cph + sex + race + region + smoking2 +
@@ -98,8 +99,9 @@ pdf(file = paste0(Save.Path,"/CCC19_Descr_Stats_Barplot.pdf"),width = 7, height 
   Plt.SexDS
   Plt.SexDS %>% FUN_Beautify_ggplot +
     scale_fill_manual(Var.set[i],values=c("#bf54a3","#5b46a3","#41548a","#45856c","#747575")) +
-    geom_text(aes(label=Number),position = position_dodge(0.95), vjust=-0.6, size=3.5,angle =45, colour="#c70e67") +
-    geom_text(aes(label=paste0(Percent,"%")),position = position_dodge(0.9), vjust=0.5, size=2,colour = "#2a2e2d",angle =45) -> Plt.SexDS_B
+    geom_text(aes(label=Number),position = position_dodge(0.95), vjust=-0.6, size=6,angle =45, colour="#c70e67") +
+    geom_text(aes(label=paste0(Percent,"%")),position = position_dodge(0.9), vjust=0.5, size=3,colour = "#2a2e2d",angle =45)+
+    ylim(0, 1300)-> Plt.SexDS_B
   Plt.SexDS_B %>% print()
   }
 dev.off()
@@ -110,9 +112,7 @@ dev.off()
 
 
 
-
-
-
+##**********************************************************************************************************************************##
 ##### Impute missing values #####
 set.seed(42)
 Imp.data0 <- aregImpute(~ dead30 + age_cph + sex + race + region + smoking2 +
@@ -145,21 +145,21 @@ Imputations <- Imp.data0$n.impute
 
 
 ## PS matching + logistic regression analysis
-models.PSM.hcq <- lapply(1:Imputations, function(im1) {
+models.PSM.rem <- lapply(1:Imputations, function(im1) {
   #im1 <- 5
   full_dat1 <- fill_data(im = im1) # form complete data
 
   full_dat1$age_cph <- full_dat1$age_cph/10 # one unit, per 10 years
 
   ## Create a dichotomous treatment variable for matching
-  full_dat1$hcq.2 <- factor(full_dat1$treatment_rem,
+  full_dat1$rem.2 <- factor(full_dat1$treatment_rem,
                                 levels=c("Negative control", "Positive control", "Rem alone", "Rem+anything"),
                                 labels = c("0", "0", "1", "1"))
 
 
   ## Start matching
   set.seed(42)
-  formula.match <- hcq.2 ~ age_cph + sex + race + region + smoking2 +
+  formula.match <- rem.2 ~ age_cph + sex + race + region + smoking2 +
                             obesitylevel + dm2 + pulm + card + renal + htn + ecogcat2 +
                             cancer_status_v2 + severity_of_covid_19_v2 + ac_apa
   match.it <- do.call(matchit, list(formula = formula.match, data = full_dat1, method = "nearest",
@@ -199,7 +199,7 @@ models.PSM.hcq <- lapply(1:Imputations, function(im1) {
 })
 
 
-RRR <- CombineImputation(models.PSM.hcq, vcov.=vcovCL, cluster = ~ subclass)
+RRR <- CombineImputation(models.PSM.rem, vcov.=vcovCL, cluster = ~ subclass)
 
 round(coefs.to.OR(RRR$fmi)$ORCI,3)
 
@@ -208,16 +208,16 @@ round(coefs.to.OR(RRR$fmi)$ORCI,3)
 
 
 
-
+##**********************************************************************************************************************************##
 ## IPTW (Inverse Probability Treatment Weighting) + logistic regression analysis
-models.IPTW.hcq <- lapply(1:Imputations, function(im1) {
+models.IPTW.rem <- lapply(1:Imputations, function(im1) {
   #im1 <- 5
   full_dat1 <- fill_data(im = im1) # form complete data
 
   full_dat1$age_cph <- full_dat1$age_cph/10 # one unit, per 10 years
 
 
-  temp <- ipwpoint(exposure = treatment_hcq,
+  temp <- ipwpoint(exposure = treatment_rem,
                    family = "multinomial",
                    numerator = ~ 1, ## must exist
                    denominator = ~ age_cph + sex + race + region + smoking2 +
@@ -233,14 +233,14 @@ models.IPTW.hcq <- lapply(1:Imputations, function(im1) {
 
   #weighteddata <- svydesign(ids = ~ 1, weights = ~ ipws, data = full_dat1)
   #weightedtable <- svyCreateTableOne(vars = colnames(full_dat1)[-c(1, 17:20)],
-  #                                   strata = "treatment_hcq", data = weighteddata, test=FALSE)
+  #                                   strata = "treatment_rem", data = weighteddata, test=FALSE)
   #sink(paste0("Table1_postIPTW_one.txt"))
   #print(weightedtable, smd=TRUE)
   #sink()
 
 
   # A formula for logistic regression analysis
-  formula1 <- dead30 ~ treatment_hcq + age_cph + sex + race + region + smoking2 +
+  formula1 <- dead30 ~ treatment_rem + age_cph + sex + race + region + smoking2 +
     obesitylevel + dm2 + pulm + card + renal + htn + ecogcat2 +
     cancer_status_v2 + severity_of_covid_19_v2 +
     ac_apa
@@ -250,7 +250,7 @@ models.IPTW.hcq <- lapply(1:Imputations, function(im1) {
   fit.bin
 })
 
-RRR <- CombineImputation(models.IPTW.hcq)
+RRR <- CombineImputation(models.IPTW.rem)
 
 round(coefs.to.OR(RRR$fmi)$ORCI,3)
 
